3fb160c6e0b07b908140d263a06393d1736681a6
Chris Karpyszyn <ckarpyszyn@uncommonpurpose.com>
2023-04-25 22:05:38 -0400

    add collision and destruction of small enemies

---
+++ Assets/PlayerInteraction.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Collider triggerCollider = GetComponent<Collider>();
+
+        if (other.CompareTag("Enemy"))
+        {
+            Vector3 triggerObjectSize = triggerCollider.transform.localScale;
+            Vector3 otherObjectSize = other.bounds.size;
+
+            //I want to know if I'm bigger or smaller than the Enemy I'm touching.
+            if (triggerObjectSize.x > otherObjectSize.x)
+            {
+                Debug.Log("I'm bigger");
+
+                //consider object pooling instead of destroying/creating
+                Destroy(other.gameObject);
+
+                float amountToChange = otherObjectSize.x / 2;
+                Debug.Log("WHAT? " + triggerCollider.transform.localScale);
+
+                triggerCollider.transform.localScale = new Vector3(amountToChange + triggerCollider.transform.localScale.x, triggerCollider.transform.localScale.y, triggerCollider.transform.localScale.z + amountToChange);
+
+
+
+                //when I'm bigger, destroy and add some size to the player
+
+            } else
+            {
+                Debug.Log("I'm smaller");
+
+                //when I'm smaller, die (or remove size?!?)
+            }
+        } 
+    }
+}
